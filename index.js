1ffb5b3d39228822bc87a4d8c21833497bcdeed5
kkg52 <kadirkaang52@gmail.com>
2023-08-28 21:13:38 +0300

    finish

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.static('public'));
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200})); // some legacy browsers choke on 204
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
@@ -13,14 +18,7 @@ app.get('/api/whoami', (req, res) => {
     const software = req.headers["user-agent"]
   
     res.json({ ipaddress, language, software })
-  })
-  
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200})); // some legacy browsers choke on 204
-
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
